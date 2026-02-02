80b933585313634313128e20ad463f75a0a737a7
PC-Dan <jeanlouis.danielo@sfr.fr>
2026-02-02 20:32:02 +0100

    Update gitignore

---
+++ pre-commit.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module ps2exe
+
+Invoke-PS2EXE `
+    -InputFile files-to-illustrator-csv.ps1 `
+    -OutputFile "Générer CSV pour Illustrator.exe" `
+    -NoConsole:$false `
+    -title:"Générateur de fichier csv compatible avec Illustrator"`
+    -description "Génère des fichiers CSV compatibles avec Adobe Illustrator" `
+    -IconFile files-to-illustrator-csv-icon.ico
